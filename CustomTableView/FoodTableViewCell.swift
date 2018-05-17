778b31f811e6a388556ac1d582fc158c484ff404
kwackjihoon <chunjoo4@naver.com>
2018-05-17 13:48:29 +0900

    add UITableViewController

---
+++ CustomTableView/FoodTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FoodTableViewCell.swift
+//  CustomTableView
+//
+//  Created by dit on 2018. 5. 17..
+//  Copyright © 2018년 dit2. All rights reserved.
+//
+
+import UIKit
+
+class FoodTableViewCell: UITableViewCell {
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
